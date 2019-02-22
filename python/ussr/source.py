203bf59dfb82f897734f20a803318f3fed576717
Spencer Griswold <sgriswold@icecube.wisc.edu>
2019-02-22 11:13:48 -0500

    Added vectorized neutrino PDF and calculation of photonic production in ice

--- python/ussr/source.py
+++ python/ussr/source.py
@@ -19,6 +19,8 @@ import numpy as np
 from scipy.special import loggamma, gdtr
 from scipy.interpolate import InterpolatedUnivariateSpline
 
+#See Pchipinterpolator
+
 
 class Source:
     
@@ -40,10 +42,22 @@ class Source:
         self.energy_pdf = lambda a, Ea, E: \
             np.exp((1 + a) * np.log(1 + a) - loggamma(1 + a) + a * np.log(E) - \
                    (1 + a) * np.log(Ea) - (1 + a) * (E / Ea))
+                   
+        self.v_energy_pdf =  np.vectorize(self.energy_pdf, excluded=['E'], signature='(1,n),(1,n)->(m,n)' )
 
         # Energy CDF, useful for random energy sampling.
         self.energy_cdf = lambda a, Ea, E: gdtr(1., a + 1., (a + 1.) * (E / Ea))
 
+    def energy_spectrum_profile(self, a, Ea, E):
+        # Returns the energy PDF discretized to every point in the space
+        # ( (a, Ea), E) Where a and Ea are n-element time-profiles of the SN Model 
+        # parameters and E is an m-element list of neutrino energies.
+        # Modifed vesion of the ufunc energy_pdf, but is compatible with vector input
+        # Very memory intensive, for default dt = 0.0001s on [-1,15] and dE = 0.1 [0,100]
+        # The returned n x m matrix of floats is 1.28 GB.
+        # Could be improved.
+        return np.exp((1 + a) * np.log(1 + a) - loggamma(1 + a) - (1 + a) * np.log(Ea)) * \
+               np.exp(np.outer(a, np.log(E)) - np.outer((1 + a)/Ea,  E)).T 
     def get_time(self):
         """Return source time as numpy array.
 .
@@ -92,7 +106,7 @@ class Source:
         return self.mean_energy[flavor](t) * u.MeV
 
 
-    def get_flux(self, t, flavor=Flavor.nu_e_bar):
+    def get_flux(self, time, flavor=Flavor.nu_e_bar):
         """Return source flux at time t for a given flavor.
 
         Parameters
@@ -108,12 +122,12 @@ class Source:
         flux : float
             Source number flux (unit-less, count of neutrinos).
         """      
-        
-        luminosity  = self.get_luminosity(t, flavor).to( u.MeV/u.s )
-        mean_energy = self.get_mean_energy(t, flavor) 
+        t = time.to(u.s).value
+        luminosity  = self.get_luminosity(t, flavor).to( u.MeV/u.s ).value
+        mean_energy = self.get_mean_energy(t, flavor).value
         
         # Where the mean energy is not zero, return rate in units neutrinos per second, elsewhere, returns zero.
-        rate = np.divide( luminosity, mean_energy, where=( mean_energy!=0 ), out=np.zeros( luminosity.size )/u.s)
+        rate = np.divide( luminosity, mean_energy, where=( mean_energy!=0 ), out=np.zeros( luminosity.size ))
         flux = np.ediff1d(t, to_end=(t[-1] - t[-2])) * rate
         
         return flux
@@ -142,11 +156,22 @@ class Source:
         # Use simple 1D linear interpolation
         a = self.pinch[flavor](t)
         Ea = self.mean_energy[flavor](t)
-        if a <= 0. or Ea <= 0.:
-            return np.zeros_like(E)
+        Enu = E.to( u.MeV ).value
+
         if E[0] == 0.:
-            E[0] = 1e-10 * u.MeV
-        return self.energy_pdf(a, Ea, E.value).real
+            E[0] = 1e-10  # u.MeV
+        
+        if isinstance(t, (list, tuple, np.ndarray)):
+            cut = (a > 0) & (Ea > 0)
+            E_pdf = np.zeros( (Enu.size, t.size), dtype = float )
+            E_pdf[:, cut] = self.v_energy_pdf( a[cut].reshape(1,-1), Ea[cut].reshape(1,-1), \
+                                               E =Enu.reshape(-1,1))
+            return E_pdf
+        else:
+            if a <= 0. or Ea <= 0.:
+                return np.zeros_like(E)
+            
+            return self.energy_pdf(a, Ea, E.value).real
 
     def sample_energies(self, t, E, n=1, flavor=Flavor.nu_e_bar):
         """Generate a random sample of neutrino energies at some time t for a
@@ -188,6 +213,29 @@ class Source:
         energies[cut] = en
 
         return energies
+        
+    def photonic_energy_per_vol(self, time, E, flavor, photon_spectrum):
+        
+        H2O_in_ice = 3.053e28 # 1 / u.m**3
+                
+        t = time.to(u.s).value
+        Enu = E.to(u.MeV)
+        if Enu[0] == 0:
+            Enu[0] = 1e-10 * u.MeV
+        phot = photon_spectrum.to(u.m**2).value.reshape((-1,1)) # m**2
+        
+        dist = self.progenitor_distance.to(u.m).value # m**2
+        flux = self.get_flux( time, flavor ) # Unitless
+        if not flavor.is_electron:
+            flux *= 2
+        
+        print('Beginning {0} simulation....'.format(flavor._name_), end='')
+        E_per_V =  np.zeros( time.size )
+        E_per_V += np.trapz( self.energy_spectrum(t, Enu, flavor) * phot, Enu.value, axis=0)
+        E_per_V *= flux * H2O_in_ice / ( 4 * np.pi * dist**2)
+        print('Completed')
+    
+        return E_per_V * u.MeV / u.m**3
 
 
 def initialize(config):
