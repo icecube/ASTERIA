6778389d7968827e7a32ed0492542e14685dcd63
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2019-05-25 11:24:41 -0400

    Optionally add SMC+LMC to mass distribution.

--- python/asteria/stellardist.py
+++ python/asteria/stellardist.py
@@ -12,6 +12,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 from astropy.io import fits
 from astropy import units as u
+from scipy.stats import norm
 from scipy.interpolate import PchipInterpolator
 
 
@@ -79,23 +80,68 @@ class FixedDistance(Distance):
 class StellarDensity(Distance):
     """Generate distances according to a Sun-centric radial mass density."""
 
-    def __init__(self, distcdf_file):
+    def __init__(self, distcdf_file, add_LMC=False, add_SMC=False):
         """Progenitor distance with some uncertainty.
         
         Parameters
         ----------
         distcdf_file : str
             File with cumulative radial stellar mass distribution w.r.t. Sun.
+        add_LMC : bool
+            If true, add a Gaussian model of the LMC stellar distribution.
+        add_SMC : bool
+            If true, add a Gaussian model of the SMC stellar distribution.
         """
         super().__init__()
 
         hdu = fits.open(distcdf_file)
-        self.dist = hdu['DIST'].data * u.Unit(hdu['DIST'].header['BUNIT'])
+        distunit = u.Unit(hdu['DIST'].header['BUNIT'])
+        self.dist = hdu['DIST'].data
         self.cdf = hdu['CDF'].data
         self.name = hdu['CDF'].header['NAME']
         self.publication = hdu['CDF'].header['PUB']
 
-        self._inv_cdf = PchipInterpolator(hdu['CDF'].data, hdu['DIST'].data)
+        # Virial mass of the MW is ~1.5e12 Msun; see L. Watkins et al., 
+        # ApJ 873:118 (2019), arXiv:1804.11348 [astro-ph.GA].
+        # Assume 5% of this is stored in stellar mass (educated guess).
+        m_MW = 0.05 * 1.5e12
+
+        if add_LMC:
+            # Combined visible mass of LMC disk and neutral gas; see 
+            # Kinematical Structure of the Magellanic System,
+            # R. P. van der Marel, N. Kallivayalil, G. Besla, Proc. IAU Symp.
+            # 256, 2009, 81-92, arXiv:0809.4268 [astro-ph].
+            m_LMC = 3.2e9
+            r_LMC = (50*u.kpc).to(distunit).value
+            sigma_LMC = (2.5*u.kpc).to(distunit).value
+
+            mratio = m_LMC/m_MW
+            dist_LMC = np.linspace(r_LMC-5*sigma_LMC, r_LMC+5*sigma_LMC, 51)
+            cdf_LMC = mratio * norm.cdf(dist_LMC, r_LMC, sigma_LMC)
+
+            self.dist = np.append(self.dist, dist_LMC)
+            self.cdf = np.append(self.cdf, 1. + cdf_LMC)
+            self.cdf /= np.max(self.cdf)
+        if add_SMC:
+            # Total stellar mass of the SMC is estimated to be 3.1x10^8 Msun;
+            # R. P. van der Marel, N. Kallivayalil, G. Besla, Proc. IAU Symp.
+            # 256, 2009, 81-92, arXiv:0809.4268 [astro-ph].
+            m_SMC = 3.1e8
+            r_SMC = (60*u.kpc).to(distunit).value
+            sigma_SMC = (1.25*u.kpc).to(distunit).value
+
+            mratio = m_SMC/m_MW
+            dist_SMC = np.linspace(r_SMC-5*sigma_SMC, r_SMC+5*sigma_SMC, 51)
+            cdf_SMC = mratio * norm.cdf(dist_SMC, r_SMC, sigma_SMC)
+
+            self.dist = np.append(self.dist, dist_SMC)
+            self.cdf = np.append(self.cdf, 1. + cdf_SMC)
+            self.cdf /= np.max(self.cdf)
+
+        self._inv_cdf = PchipInterpolator(self.cdf, self.dist)
+
+        # Set distance in distance units.
+        self.dist *= distunit
 
     def distance(self, size=1):
         """Generate distance to a progenitor.
