837ea19ece252aa3fec65565858554b845279420
Spencer Griswold <sgriswold@icecube.wisc.edu>
2019-07-09 13:40:53 -0500

    Added detection for directories scratch/ and data/processed/

--- python/asteria/__init__.py
+++ python/asteria/__init__.py
@@ -9,3 +9,19 @@ energy spectra data files.
 """
 from __future__ import absolute_import
 from ._version import __version__
+import os
+
+base_path = os.environ['ASTERIA']
+local_path = os.path.realpath(__file__)
+
+# Test for environment variable ASTERIA
+if base_path not in local_path:
+    raise OSError('Environment variable ASTERIA not set properly')
+
+# Test for existence of scratch directory
+if not os.path.isdir(base_path + '/scratch'):
+    os.mkdir(base_path + '/scratch')
+
+# Test for existence of processed directory
+if not os.path.isdir(base_path + '/data/processed'):
+    os.mkdir(base_path + '/data/processed')
