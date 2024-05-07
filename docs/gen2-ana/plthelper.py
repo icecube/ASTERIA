419aa4f67ad623959aa7a1587e17749635a1b521
jakob2508 <jakob.beise@physics.uu.se>
2024-05-07 12:07:14 +0200

    improved plot_significance and added parameter scan plot

--- docs/gen2-ana/plthelper.py
+++ docs/gen2-ana/plthelper.py
@@ -38,7 +38,10 @@ def plot_fft(freq, power, det = "ic86"):
     plt.tight_layout()
 
 
-def plot_stft(time ,freq, power, det = "ic86"):
+def plot_stft(time, freq, power, det = "ic86"):
+
+    freq = freq.value
+    time = time.value
 
     hypos = ["signal", "null"]
     im, cb = [None,None], [None,None]
@@ -46,7 +49,7 @@ def plot_stft(time ,freq, power, det = "ic86"):
     fig, ax = plt.subplots(1,2, figsize = (10,4))
 
     for i in range(2):
-        im[i] = ax[i].pcolormesh(time, freq, np.log(power[hypos[i]][det][0]), cmap='plasma', shading = "nearest")
+        im[i] = ax[i].pcolormesh(time, freq, np.log(power[hypos[i]][det][0]), cmap='plasma', shading = "nearest", vmin = 0, vmax = 8)
         
         cb = fig.colorbar(im[i])
         cb.ax.tick_params(labelsize=14)
@@ -58,7 +61,10 @@ def plot_stft(time ,freq, power, det = "ic86"):
 
     plt.tight_layout()
 
-def plot_stft_log(time ,freq, log_power, det = "ic86"):
+def plot_stft_log(time, freq, log_power, det = "ic86"):
+
+    freq = freq.value
+    time = time.value
 
     hypos = ["signal", "null"]
     im, cb = [None,None], [None,None]
@@ -79,6 +85,8 @@ def plot_stft_log(time ,freq, log_power, det = "ic86"):
     plt.tight_layout()
 
 def plot_stft_time_int(freq, log_int, det = "ic86"):
+
+    freq = freq.value
 
     diff = log_int["signal"][det][0] - log_int["null"][det][0]
     
@@ -166,7 +174,7 @@ def plot_fit_freq(fit_freq, det = "ic86"):
     ax.axvline(np.median(fit_freq_null), color="C0")
     ax.axvline(np.median(fit_freq_signal), color="C1")
     ax.set_xlabel("Frequency [Hz]", fontsize=14)
-    ax.set_xlabel("Counts", fontsize=14)
+    ax.set_ylabel("Counts", fontsize=14)
     ax.set_yscale("log")
     ax.tick_params(labelsize=14)
     ax.legend(fontsize = 14)
@@ -227,45 +235,13 @@ def plot_significance(dist_range, zscore, ts_stat):
     labels = [r'$IceCube$', r'$Gen2$', r'$Gen2+WLS$']
     colors = ['C0', 'C1', 'C2']
 
+    mask = np.where(np.isinf(zscore["ic86"][2])==True, False, True)
+
     for i, det in enumerate(["ic86", "gen2", "wls"]):
 
         ax[0].plot(dist_range, zscore[det][0], label=labels[i], color = colors[i])
         ax[0].fill_between(dist_range.value, zscore[det][2], zscore[det][1], color = colors[i], alpha = 0.15)
-
-        ax[0].set_xlabel('Distance d [kpc]', fontsize = 12)
-        ax[0].set_ylabel(r'SASI detection significance [$\sigma$]' , fontsize = 12)
-        #ax[0].set_xlim((1,20))
-        ax[0].set_ylim((1,30))
-
-        ax[0].tick_params(labelsize = 12)
-
-        ax[0].set_yscale('log')
-        ax[0].set_yticks([1,2,3,5,10,20,30])
-        ax[0].set_yticklabels(['1','2','3','5','10','20','30'])
-
-        ax[0].grid()
-        ax[0].legend(loc='upper right', fontsize = 12)
-
-        ax[0].axhline(3, color='k', ls = ':')
-        ax[0].axhline(5, color='k', ls = '-.')
-        ax[0].text(8, 3, r"3$\sigma$", size=12,
-                ha="center", va="center",
-                bbox=dict(boxstyle="square", ec='k', fc='white'))
-
-        ax[0].text(8, 5, r"5$\sigma$", size=12,
-                ha="center", va="center",
-                bbox=dict(boxstyle="square", ec='k', fc='white'))
-
-        # distance for CDF value of 0.1, 0.5, etc.
-        cdf_val = np.array([0.1,0.5,0.75,0.9,0.95,0.99,1])
-        stellar_dist = coverage_to_distance(cdf_val).flatten()
-
-        ax22 = ax[0].twiny()
-        ax22.set_xlim(ax[0].get_xlim())
-        ax22.set_xticks(stellar_dist)
-        ax22.set_xticklabels((cdf_val*100).astype(dtype=int), rotation = 0, fontsize = 12)
-        ax22.set_xlabel('Cumulative galactic CCSNe distribution \n from Adams et al. (2013) in [%]', fontsize = 12)
-
+    
         #signal without errorbar
         ax[1].plot(dist_range, ts_stat["signal"][det][0], color = colors[i], label=r'TS$^{sig}_{IceCube}$')
 
@@ -274,12 +250,44 @@ def plot_significance(dist_range, zscore, ts_stat):
                     yerr = (ts_stat["null"][det][0]-ts_stat["null"][det][1],ts_stat["null"][det][2]-ts_stat["null"][det][0]), 
                     capsize=4, ls = ':', color = colors[i], label=r'TS$^{bkg}_{IceCube}$')
 
+    ax[0].set_xlabel('Distance d [kpc]', fontsize = 12)
+    ax[0].set_ylabel(r'SASI detection significance [$\sigma$]' , fontsize = 12)
+    #ax[0].set_xlim((1,20))
+    ax[0].set_ylim((1,30))
+
+    ax[0].tick_params(labelsize = 12)
+
+    ax[0].set_yscale('log')
+    ax[0].set_yticks([1,2,3,5,10,20,30])
+    ax[0].set_yticklabels(['1','2','3','5','10','20','30'])
+
+    ax[0].grid()
+    ax[0].legend(loc='upper right', fontsize = 12)
+
+    ax[0].axhline(3, color='k', ls = ':')
+    ax[0].axhline(5, color='k', ls = '-.')
+    ax[0].text(dist_range[mask][0].value, 3, r"3$\sigma$", size=12,
+            ha="center", va="center",
+            bbox=dict(boxstyle="square", ec='k', fc='white'))
+
+    ax[0].text(dist_range[mask][0].value, 5, r"5$\sigma$", size=12,
+            ha="center", va="center",
+            bbox=dict(boxstyle="square", ec='k', fc='white'))
+
+    # distance for CDF value of 0.1, 0.5, etc.
+    cdf_val = np.array([0.1,0.5,0.75,0.9,0.95,0.99,1])
+    stellar_dist = coverage_to_distance(cdf_val).flatten()
+
+    ax22 = ax[0].twiny()
+    ax22.set_xlim(ax[0].get_xlim())
+    ax22.set_xticks(stellar_dist)
+    ax22.set_xticklabels((cdf_val*100).astype(dtype=int), rotation = 0, fontsize = 12)
+    ax22.set_xlabel('Cumulative galactic CCSNe distribution \n from Adams et al. (2013) in [%]', fontsize = 12)
+
     #rearrange legend handels
     handles,labels = ax[1].get_legend_handles_labels()
-
     handles = [handles[0], handles[3], handles[1], handles[4], handles[2], handles[5]]
     labels = [labels[0], labels[3], labels[1], labels[4], labels[2], labels[5]]
-
 
     ax[1].set_xlabel('Distance d [kpc]', fontsize = 12)
     ax[1].set_ylabel('Median of test statistics (TS)', fontsize = 12)
@@ -288,5 +296,19 @@ def plot_significance(dist_range, zscore, ts_stat):
     ax[1].tick_params(labelsize = 12)
     ax[1].legend(handles, labels, ncol = 3, fontsize = 12, bbox_to_anchor=(0.13, 1))
     ax[1].grid()
+    plt.tight_layout()
+
+def plot_para_scan(freq_range, ampl_range, data):
+
+    fig, ax = plt.subplots(1,1, figsize = (10,4))
+
+    im = ax.pcolormesh(freq_range.value, ampl_range*100, data, cmap="viridis", shading="nearest")
+    cb = fig.colorbar(im)
+    cb.ax.tick_params(labelsize=14)
+    cb.set_label(r"3 $\sigma$ Distance horizon [kpc]", size=14)
+    ax.set_xlabel(r'Frequency in [Hz]', fontsize=14)
+    ax.set_ylabel(r'Amplitude [%]', fontsize=14)
+    ax.yaxis.get_offset_text().set_fontsize(14)
+    ax.tick_params(labelsize=14)
 
     plt.tight_layout()
