4efab287c6608aa455e7cf0b5281e484edce422a
jakob2508 <jakob.beise@physics.uu.se>
2024-05-07 12:04:20 +0200

    replaced minimizer with bentq alogorithm which is more stable

--- docs/gen2-ana/helper.py
+++ docs/gen2-ana/helper.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 from asteria.stellardist import StellarDensity
-from scipy.interpolate import PchipInterpolator
-from scipy.optimize import minimize
+from scipy.interpolate import PchipInterpolator, InterpolatedUnivariateSpline
+from scipy.optimize import brentq
 
 import astropy.units as u
 
@@ -30,7 +30,6 @@ def moving_average(a, n=3, zero_padding = False, const_padding = False):
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
-
 def loss_stellar(dist, cdf_val):
     # for distances above 25 kpc the CDF = 1, but we want hit the 'edge'
     # we punish any value that is higher than 25 kpc + epsilon, epsilon << 1
@@ -38,7 +37,7 @@ def loss_stellar(dist, cdf_val):
     if dist > 25 + 1E-3:
         return 1E6
     else:
-        loss = np.abs(stellar_inter(dist) - cdf_val)
+        loss = (stellar_inter(dist) - cdf_val)
         return loss
 
 def distance_to_coverage(dist):
@@ -50,73 +49,54 @@ def coverage_to_distance(cdf_val):
 
     # for single value
     if not isinstance(cdf_val, np.ndarray):
-        res = minimize(loss_stellar, x0 = 5, args=cdf_val)
-        return res.x
+        root = brentq(loss_stellar, a = 1, b = 100, args = cdf_val, xtol = 1e-2)
+        return root
     
     # for an array loop through the entries
     else:
         dist = [] # empty distance array
         for cv in cdf_val:
-            loss, i = 1, 0 # initialize loss and counter
-            x0 = np.array([5,10,15,20]) # list of initial guess
-            while loss > 1E-3: # repeat as long as loss > 0.001
-                res = minimize(loss_stellar, x0 = x0[i], args = cv)
-                loss = res.fun
-                i += 1
-                if i > 3:
-                    print('Fit failed to converge!')
-                    break
-            dist.append(res.x)
-        dist = np.array(dist)
-        return dist
-    
+            root = brentq(loss_stellar, a = 1, b = 100, args = cv, xtol = 1e-2)
+            dist.append(root)
+        return np.array(dist)
+
 def loss_dist_horizon(dist, quan, sig):
-    return np.sqrt(np.sum((quan(dist)-sig)**2))
+    return (quan(dist)-sig)
 
-def significance_horizon(dist_range, Zscore, confidence_level = [3,5]):
+def significance_horizon(dist_range, Zscore, sigma = [3,5]):
                         
-    sigma = [str(cl) + "sig" for cl in confidence_level] 
-
-    dist = {key : {"ic86": None, "gen2": None, "wls": None} for key in sigma} # empty dictionary
-    perc = {key : {"ic86": None, "gen2": None, "wls": None} for key in sigma}
+    dist = {"ic86": [], "gen2": [], "wls": []} # empty dictionary
+    perc = {"ic86": [], "gen2": [], "wls": []}
                         
 
     for det in ["ic86", "gen2", "wls"]: # loop over detector
-
+        print(det)
         dist_min = 0.5 * u.kpc
 
         # cutoff distance for interpolation, inf values need to be excluded, take 86% and first distance
         dist_cutoff = dist_range[np.logical_and(dist_range>dist_min, np.where(np.isinf(Zscore[det][2])==True, 0, 1))][0]
-
-        # Interpolate Zscore data for 16%, 50% and 84% quantiles
-        q50 = PchipInterpolator(dist_range[dist_range>dist_cutoff], Zscore[det][0][dist_range>dist_cutoff])
-        q16 = PchipInterpolator(dist_range[dist_range>dist_cutoff], Zscore[det][1][dist_range>dist_cutoff])
-        q84 = PchipInterpolator(dist_range[dist_range>dist_cutoff], Zscore[det][2][dist_range>dist_cutoff])
+        
+        # cubic spline (k = 3) with constant values outside boundaries
+        q50 = InterpolatedUnivariateSpline(x = dist_range[dist_range>=dist_cutoff], y = Zscore[det][0][dist_range>=dist_cutoff], k = 3, ext = 3)
+        q16 = InterpolatedUnivariateSpline(x = dist_range[dist_range>=dist_cutoff], y = Zscore[det][1][dist_range>=dist_cutoff], k = 3, ext = 3)
+        q84 = InterpolatedUnivariateSpline(x = dist_range[dist_range>=dist_cutoff], y = Zscore[det][2][dist_range>=dist_cutoff], k = 3, ext = 3)
         
         quantiles = [q50, q16, q84]
 
-        for i, cl in enumerate(confidence_level): # loop over confidence level (e.g. 3, 5 sigma)
-            x0 = dist_cutoff.value #???
+        for sig in sigma: # loop over confidence level (e.g. 3, 5 sigma)
             di, pe = [], [] # temporary lists to store distance and percentage
 
             for quan in quantiles: # loop over quantiles
-                loss = 1
-                while loss > 1E-3:
-                    res = minimize(loss_dist_horizon, x0=x0, args = (quan, cl))
-                    loss = res.fun
-                    x0 += 1
-                    if x0 > 50:
-                        print('Minimization did not converge')
-                        break
-                di.append(res.x[0])
-                if res.x >= 25:
+
+                root = brentq(loss_dist_horizon, a = 1, b = 100, args = (quan, sig), xtol = 1e-2)
+                di.append(root)
+                if root >= 25:
                     pe.append(1)
                 else:
-                    pe.append(stellar_inter(res.x[0]))
+                    pe.append(stellar_inter(root))
                 
-            dist[sigma[i]][det] = np.array(di) * u.kpc
-            perc[sigma[i]][det] = np.array(pe)*100
-
+            dist[det].append(np.array(di) * u.kpc)
+            perc[det].append(np.array(pe)*100)
         
     return dist, perc
 
