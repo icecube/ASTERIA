4a4abef35e3c599a45e1b0c39ac1dea1bde90009
jakob2508 <jakob.beise@physics.uu.se>
2024-06-07 11:21:01 +0200

    loop over non-integer, integer values will not be run

--- docs/gen2-ana/seperate/run_background_trials.sh
+++ docs/gen2-ana/seperate/run_background_trials.sh
@@ -4,7 +4,9 @@ OUTPUTVAL=$1
 
 SAMPLES=100000
 
-for DIST in {1..2}
-do
-    python run_background_trials.py $DIST $SAMPLES $OUTPUTVAL
+for DIST in $(seq 1 0.2 2); do
+
+    if [[ $(echo $DIST | awk -F. '{print $2}') != 0 ]]; then
+        python run_background_trials.py $DIST $SAMPLES $OUTPUTVAL
+    fi
 done
