eed4888976ce06e245a017606605780af6a6ee3c
jakob2508 <jakob.beise@physics.uu.se>
2024-06-11 17:23:23 +0200

    loop over distance instead of output

--- docs/gen2-ana/seperate/run_background_trials.sh
+++ docs/gen2-ana/seperate/run_background_trials.sh
@@ -2,11 +2,8 @@
 
 OUTPUTVAL=$1
 
-SAMPLES=100000
+SAMPLES=100000000
 
-for DIST in $(seq 1 0.2 2); do
-
-    if [[ $(echo $DIST | awk -F. '{print $2}') != 0 ]]; then
-        python run_background_trials.py $DIST $SAMPLES $OUTPUTVAL
-    fi
+for ((IDIST=0; i<296; i++))
+    python run_background_trials.py $IDIST $SAMPLES 0
 done
