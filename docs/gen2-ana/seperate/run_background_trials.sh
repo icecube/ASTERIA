b9ef556ebe63f96088c0d8e799ef352799195605
jakob2508 <jakob.beise@physics.uu.se>
2024-07-02 17:28:28 +0200

    removed .sh files from commit

--- docs/gen2-ana/seperate/run_background_trials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUTPUTVAL=$1
-
-SAMPLES=100000000
-
-for ((IDIST=0; i<296; i++))
-    python run_background_trials.py $IDIST $SAMPLES 0
-done
--- docs/gen2-ana/seperate/run_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-trials=10000
-
-for ((i=10; i<=100; i+=10))
-do
-    python run_scan.py $i $trials
-done
